67dbaf167bd52151a20ad7df028ef6cd318e43d2
articulars <50593308+articulars@users.noreply.github.com>
2019-06-19 15:23:10 -0700

    Update HTU21D_test.c

--- HTU21D_test.c
+++ HTU21D_test.c
@@ -10,16 +10,21 @@
 
 int main ()
 {
-	wiringPiSetup();
-	int fd = wiringPiI2CSetup(HTU21D_I2C_ADDR);
-	if ( 0 > fd )
-	{
-		fprintf (stderr, "Unable to open I2C device: %s\n", strerror (errno));
-		exit (-1);
-	}
-	
-	printf("%5.2fC\n", getTemperature(fd));
-	printf("%5.2f%%rh\n", getHumidity(fd));
+	int i;
+       for(i=0; i<5; i++) 
+       {
+		wiringPiSetup();
+		int fd = wiringPiI2CSetup(HTU21D_I2C_ADDR);
+		if ( 0 > fd )
+		{
+			fprintf (stderr, "Unable to open I2C device: %s\n", strerror (errno));
+			exit (-1);
+		}
+
+		printf("%5.2fC\n", getTemperature(fd));
+		printf("%5.2f%%rh\n", getHumidity(fd));
+	       sleep(1);
+       }
 	
 	return 0;
 }
